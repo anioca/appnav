78f601197e6f695ca27a099cf9ed409e27d540dd
Your Name <you@example.com>
2024-02-29 16:30:40 -0300

    inicial

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,32 @@
+import { ScrollView, Text, View } from "react-native";
+import styles from "../config/styles";
+
+// export default function já declara a exportação do
+// componente para ser usado posteriormente
+export default function HomeScreen() {
+  // Estrutura básica de um componente
+  // precisa ter um retorno
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+      </ScrollView>
+    </View>
+  );
+}
+
+/*
+ Eu não preciso da linha abaixo
+ pois eu declarei a função direta como
+ sendo uma função de export default
+*/
+// export default HomeScreen
+
+
+
+
